0cd3f9bad12fa78842827508d319b7ffd9982b69
imyourm8 <imyourm9@gmail.com>
2022-03-03 13:03:14 +0700

    chg: transfer ownership of the governance to itself

--- packages/core/contracts/common/governance/Governance.sol
+++ packages/core/contracts/common/governance/Governance.sol
@@ -1,10 +1,18 @@
 pragma solidity ^0.8.0;
 
-import  "./IGovernance.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "./IGovernance.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-contract Governance is Ownable, IGovernance {
-    function update(address target, bytes calldata data) external override onlyOwner {
+contract Governance is OwnableUpgradeable, IGovernance {
+    function initialize() external initializer {
+        __Ownable_init();
+    }
+
+    function update(address target, bytes calldata data)
+        external
+        override
+        onlyOwner
+    {
         (bool success, ) = target.call(data); /* bytes memory returnData */
         require(success, "Update failed");
     }
