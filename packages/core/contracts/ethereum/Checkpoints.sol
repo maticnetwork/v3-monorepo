27c9bcf1630b07d60ad76fcf4e4b30c73afaa195
imyourm8 <imyourm9@gmail.com>
2022-03-03 13:04:23 +0700

    new: draft cehckpoints contract

--- packages/core/contracts/ethereum/Checkpoints.sol
+++ packages/core/contracts/ethereum/Checkpoints.sol
@@ -1,7 +1,93 @@
 pragma solidity ^0.8.0;
 
-contract Checkpoints {
-  event Checkpoint();
+import "../common/mixin/Governable.sol";
 
-  function submitCheckpoint() external {}
+contract Checkpoints is Governable {
+    /// @dev Explain to a developer any extra details
+    /// @param proposer Address of the checkpoint proposer
+    /// @param checkpointId Id of the checkpoint
+    /// @param start Start of the block range in the checkpoint
+    /// @param end End of the block range in the checkpoint
+    /// @param root Root hash of the block range
+    event CheckpointSubmitted(
+        address indexed proposer,
+        uint256 indexed checkpointId,
+        uint256 start,
+        uint256 end,
+        bytes32 root
+    );
+
+    struct Checkpoint {
+        bytes32 root;
+        uint256 start;
+        uint256 end;
+        uint256 createdAt;
+        address proposer;
+    }
+
+    uint256 public constant CHAIN_ID = 15001;
+    uint256 public checkpointId;
+
+    mapping(uint256 => Checkpoint) public checkpoints;
+
+    function submit(
+        bytes calldata data,
+        uint8[] memory _v,
+        bytes32[] memory _r,
+        bytes32[] memory _s
+    ) external {
+        (
+            address proposer,
+            uint256 start,
+            uint256 end,
+            bytes32 rootHash,
+            bytes32 accountHash,
+            uint256 chainId
+        ) = abi.decode(
+                data,
+                (address, uint256, uint256, bytes32, bytes32, uint256)
+            );
+        require(CHAIN_ID == chainId, "Invalid bor chain id");
+
+        _validateAndSubmitCheckpoint(
+          proposer,
+          start,
+          end,
+          rootHash
+        );
+    }
+
+    /** PRIVATE METHODS */
+
+    function _validateAndSubmitCheckpoint(
+        address proposer,
+        uint256 start,
+        uint256 end,
+        bytes32 rootHash
+    ) private returns (bool) {
+        uint256 startBorBlock;
+        uint256 _checkpointId = checkpointId;
+
+        if (_checkpointId != 0) {
+            // not a first checkpoint, 
+            startBorBlock = checkpoints[_checkpointId].end + 1;
+        }
+        if (startBorBlock != start) {
+            revert("incorrect start block");
+        }
+
+        Checkpoint memory checkpoint = Checkpoint({
+            root: rootHash,
+            start: startBorBlock,
+            end: end,
+            createdAt: block.timestamp,
+            proposer: proposer
+        });
+
+        checkpoints[_checkpointId] = checkpoint;
+
+        emit CheckpointSubmitted(proposer, _checkpointId, start, end, rootHash);
+
+        checkpointId = _checkpointId + 1;
+    }
 }
